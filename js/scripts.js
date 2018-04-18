b559b2f5971529c9cab2cf37c2a9b0978486b97d
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2018-04-17 23:21:36 -0700

    Update scripts.js
    Added several variables, added change listener to jobRole drop-down to show text field when other is selected, added change listener to design so that the color drop down only shows options available for the selected design, started work on a change listener for the activities section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
+const $jobRole = $("#title");
+const $otherTitle = $("#other-title");
+const $design = $("#design");
+const $jsPuns = $(".jsPuns");
+const $heartJS = $(".heartJS");
+const $activities = $(".activities");
+
+// hides text field for 'other' job role
+$otherTitle.hide();
+// hides color dropdown in shirt fieldset
+$jsPuns.hide();
+$heartJS.hide();
 
 // gives focus to the first text field on page load
 function focusName() {
-    document.querySelector("[for='name']").focus();
+    const $name = $("label[for='name']");
+    $name.focus();
 }
 
+// shows text input if the user selects "other" from the Job Role dropdown
+$($jobRole).change(function() {
+    if ($jobRole.val() === "other") {
+        $otherTitle.show().focus();
+    } else {
+        $otherTitle.hide();
+    }
+});
+
+// shows colors appropriate to which design is selected
+$($design).change(function () {
+    $jsPuns.hide();
+    $heartJS.hide();
+
+    if ($design.val() === "js puns") {
+        $jsPuns.show();
+    }
+    if ($design.val() === "heart js") {
+        $heartJS.show();
+    }
+});
+
+$($activities).change(function() {
+    console.log("change");
+
+});
+
+// calls function on page load
 focusName();
